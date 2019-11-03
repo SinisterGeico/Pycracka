2a32aacb189a3766721ca5a6ee45236fbbcb3617
SinisterGeico <57330245+SinisterGeico@users.noreply.github.com>
2019-11-03 15:33:59 -0600

    Update hash_generator.py

--- hash_generator.py
+++ hash_generator.py
@@ -3,21 +3,26 @@ import time
 
 from termcolor import colored, cprint
 
-
+#wait function
 def wait(amount):
     time.sleep(amount)
     print()
 
-
+#variables
 password = ""
 algo = ""
-
+#startup
 print(colored("Welcome to Hasher","cyan"))
 cprint("Made By Alex Haug", "green", attrs=['bold', 'underline', 'blink'])
+cprint("v1.0", "blue", attrs=['bold', 'underline', 'blink'])
 wait(0.5)
+# what you want to hash
 print("Please enter the password to hash")
 password = input()
 print()
+
+# Pick algoritm to hash your password
+#print options
 print(colored("Please pick the number corresponding to your desired hash algorithm", "magenta"))
 wait(1)
 print(colored("1. MD-5", "green"))
@@ -32,6 +37,7 @@ print(colored("5. SHA-384", "green"))
 time.sleep(0.5)
 print(colored("6. SHA-512", "green"))
 wait(1)
+# select an option
 print((colored("Please enter that number", "yellow")))
 selection = int(input())
 print()
@@ -53,8 +59,9 @@ elif selection == 5:
 elif selection == 6:
     hash_object = hashlib.sha512(password .encode('utf-8'))
     algo = "SHA-512"
-
+    
+#hexidecimal format of hash
 hex_dig = hash_object.hexdigest()
-
+#print hash
 print("The " + algo + " hash for " + password + " is ", colored(hex_dig, "red"))
 print("Have a Nice Day. Bye.")
