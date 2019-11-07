ad7beca773fa302204521649b03b9f6419f185b8
SinisterGeico <57330245+SinisterGeico@users.noreply.github.com>
2019-11-07 15:19:07 -0600

    Update and rename hash_generator.py to HashGen.py

--- HashGen.py
+++ HashGen.py
@@ -1,20 +1,23 @@
-import hashlib
+iimport hashlib
 import time
 
 from termcolor import colored, cprint
 
-#wait function
+
+# wait function
 def wait(amount):
     time.sleep(amount)
     print()
 
-#variables
+
+# variables
 password = ""
 algo = ""
-#startup
-print(colored("Welcome to Hasher","cyan"))
+# startup
+print(colored("Welcome to Hasher", "cyan"))
 cprint("Made By Alex Haug", "green", attrs=['bold', 'underline', 'blink'])
 cprint("v1.0", "blue", attrs=['bold', 'underline', 'blink'])
+cprint('https://github.com/SinisterGeico/Pycracka')
 wait(0.5)
 # what you want to hash
 print("Please enter the password to hash")
@@ -22,7 +25,7 @@ password = input()
 print()
 
 # Pick algoritm to hash your password
-#print options
+# print options
 print(colored("Please pick the number corresponding to your desired hash algorithm", "magenta"))
 wait(1)
 print(colored("1. MD-5", "green"))
@@ -42,26 +45,26 @@ print((colored("Please enter that number", "yellow")))
 selection = int(input())
 print()
 if selection == 1:
-    hash_object = hashlib.md5(password .encode('utf-8'))
+    hash_object = hashlib.md5(password.encode('utf-8'))
     algo = "MD-5"
 elif selection == 2:
-    hash_object = hashlib.sha1(password .encode('utf-8'))
+    hash_object = hashlib.sha1(password.encode('utf-8'))
     algo = "SHA-1"
 elif selection == 3:
-    hash_object = hashlib.sha224(password .encode('utf-8'))
+    hash_object = hashlib.sha224(password.encode('utf-8'))
     algo = "SHA-224"
 elif selection == 4:
-    hash_object = hashlib.sha256(password .encode('utf-8'))
+    hash_object = hashlib.sha256(password.encode('utf-8'))
     algo = "SHA-256"
 elif selection == 5:
-    hash_object = hashlib.sha384(password .encode('utf-8'))
+    hash_object = hashlib.sha384(password.encode('utf-8'))
     algo = "SHA-384"
 elif selection == 6:
-    hash_object = hashlib.sha512(password .encode('utf-8'))
+    hash_object = hashlib.sha512(password.encode('utf-8'))
     algo = "SHA-512"
-    
-#hexidecimal format of hash
+
+# hexidecimal format of hash
 hex_dig = hash_object.hexdigest()
-#print hash
+# print hash
 print("The " + algo + " hash for " + password + " is ", colored(hex_dig, "red"))
 print("Have a Nice Day. Bye.")
